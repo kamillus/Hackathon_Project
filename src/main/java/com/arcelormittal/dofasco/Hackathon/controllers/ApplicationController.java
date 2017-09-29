71b1ccaf87edeb1ec1b4f6c27ae86628cbd32bfb
Awid <kamil.awid@arcelormittal.com>
2017-09-29 09:43:36 -0400

    Changes..

--- src/main/java/com/arcelormittal/dofasco/Hackathon/controllers/ApplicationController.java
+++ src/main/java/com/arcelormittal/dofasco/Hackathon/controllers/ApplicationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by kawid on 9/28/2017.
  * adding a comment to support 0eee8fe80f250300470d47bce1050ef5
- * Some change
+ * Some change.. another test
  */
 @Controller
 public class ApplicationController {
--- src/test/java/com/arcelormittal/dofasco/Hackathon/Project/HackathonProjectApplicationTests.java
+++ src/test/java/com/arcelormittal/dofasco/Hackathon/Project/HackathonProjectApplicationTests.java
@@ -23,5 +23,5 @@ public class HackathonProjectApplicationTests {
 	{
 		assertEquals("hello", a.helloAction());
 	}
-	
+
 }
