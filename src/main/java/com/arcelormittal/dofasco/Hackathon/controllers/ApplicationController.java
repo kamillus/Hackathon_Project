4943132f41f1f028b0465459d28129662f5b29c9
Awid <kamil.awid@arcelormittal.com>
2017-09-28 21:41:54 -0400

    Adding new change

--- src/main/java/com/arcelormittal/dofasco/Hackathon/controllers/ApplicationController.java
+++ src/main/java/com/arcelormittal/dofasco/Hackathon/controllers/ApplicationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by kawid on 9/28/2017.
+ * adding a comment to support 0eee8fe80f250300470d47bce1050ef5
  */
 @Controller
 public class ApplicationController {
